67d521369dd0938e8bc3140312c4573f3c9ae8bb
次毅 <200954381@qq.com>
2016-01-06 21:29:44 +0800

    contorllers层的初步处理

--- testTools/routes/index.js
+++ testTools/routes/index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var
+var index = require('./controllers/index.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('post');
+router.post('post', index.post);
 
 module.exports = router;
